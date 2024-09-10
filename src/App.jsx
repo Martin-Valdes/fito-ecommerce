6f1592d896c6beb36634ddf7262acaca5438ca8e
fernando valdes <valdesmartin@gmail.com>
2024-09-09 21:57:19 -0300

    footer and home are ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomeContainer from './components/HomeContainer/HomeContainer.jsx';
 import NavBar from './components/NavBar/NavBar.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 import "./App.css";
 
@@ -12,6 +13,7 @@ function App() {
       <div>
         <NavBar/>
        <HomeContainer/>
+      <Footer/>
       </div>
     </>
   );
