c5b77ca5969c9fb81a879b62f8117ccfaa2e7484
Fernando Valdes <140764694+Martin-Valdes@users.noreply.github.com>
2024-09-25 14:11:41 -0300

    responsive homecontainer and others functions with the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { authContext } from "../../Context/AuthContext";
 import CartWidget from "./CartWidget";
