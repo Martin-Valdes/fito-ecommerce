7d660c1aee9bad285776f9450770a7521cad80a3
fernando valdes <valdesmartin@gmail.com>
2024-10-07 14:39:00 -0300

    Primer commit de adoptme

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,40 +5,57 @@ import "./Contact.scss";
 const Contact = () => {
   return (
     <div className="contactContainer">
-      <section className="blocksContainer">
-        <div></div>
-        <div></div>
-        <div></div>
-      </section>
-      <section className="formContainer">
-        <div className="titleContainer">
-          <h1>Envianos tu consulta</h1>
-        </div>
-        <input
-          className="formInput inp"
-          type="text"
-          id="nameProduct"
-          name="nameProduct"
-          placeholder="Nombre"
-        />
-        <input
-          className="formInput inp"
-          type="text"
-          id="nameProduct"
-          name="nameProduct"
-          placeholder="email"
-        />
-        <textarea
-          class="formInput message"
-          id="message"
-          rows="10"
-          name="message"
-          placeholder="En que podemos ayudarte..."
-        ></textarea>
-        <div className="buttonContainer">
-          <button className="buttonSend">Enviar</button>
-        </div>
-      </section>
+      <div className="back">
+      </div>
+      <div className="filter"></div>
+      <div className="new">
+          <section className="blocksContainer">
+            <div className="block">
+              <img src="" alt="" />
+              <h2>Email</h2>
+              <p>fitogrowshop@gmail.com</p>
+            </div>
+            <div className="block">
+              <img src="" alt="" />
+              <h2>Direccion</h2>
+              <p>Toledo  Canelones 275 Bis.</p>
+            </div>
+            <div className="block">
+              <img src="" alt="" />
+              <h2>Telefono</h2>
+              <p>(+598) 094 588 350</p>
+            </div>
+          </section>
+        <section className="formContainer">
+          <div className="titleContainer">
+            <h1>Envianos tu consulta</h1>
+          </div>
+          <input
+            className="formInput inp"
+            type="text"
+            id="nameProduct"
+            name="nameProduct"
+            placeholder="Nombre"
+          />
+          <input
+            className="formInput inp"
+            type="text"
+            id="nameProduct"
+            name="nameProduct"
+            placeholder="email"
+          />
+          <textarea
+            class="formInput message"
+            id="message"
+            rows="10"
+            name="message"
+            placeholder="En que podemos ayudarte..."
+          ></textarea>
+          <div className="buttonContainer">
+            <button className="buttonSend">Enviar</button>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
