fa84858651ac65d030a91cbee8fab89b49cf9d58
fernando valdes <valdesmartin@gmail.com>
2024-10-06 22:21:15 -0300

    Correcciones en ortografia

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+
+import "./Contact.scss";
 
 const Contact = () => {
   return (
-    <div>
-      <p>Contact</p>
+    <div className="contactContainer">
+      <section className="blocksContainer">
+        <div></div>
+        <div></div>
+        <div></div>
+      </section>
+      <section className="formContainer">
+        <div className="titleContainer">
+          <h1>Envianos tu consulta</h1>
+        </div>
+        <input
+          className="formInput inp"
+          type="text"
+          id="nameProduct"
+          name="nameProduct"
+          placeholder="Nombre"
+        />
+        <input
+          className="formInput inp"
+          type="text"
+          id="nameProduct"
+          name="nameProduct"
+          placeholder="email"
+        />
+        <textarea
+          class="formInput message"
+          id="message"
+          rows="10"
+          name="message"
+          placeholder="En que podemos ayudarte..."
+        ></textarea>
+        <div className="buttonContainer">
+          <button className="buttonSend">Enviar</button>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/ServicesComponent/Services.jsx
+++ src/components/ServicesComponent/Services.jsx
@@ -7,15 +7,21 @@ const Services = () => {
     <div className="servicesContainer">
       <section className="prensCannavis">
         <h1>Servicio de prensado</h1>
-        <p>
-          El proceso comienza con la entrega de la materia prima en su estado
-          óptimo por parte del cultivador. Esta materia se prepara
-          cuidadosamente para ser colocada en una malla de diferentes micras,
-          dependiendo del nivel de filtración deseado, o en un papel siliconado
-          especializado. A continuación, se somete a un prensado utilizando
-          equipos hidráulicos de alta precisión.
-          <br />
-          <p></p>
+        <h2>El Arte de la Extracción de Rosin</h2>
+        <div className="firstContainer">
+          <p className="firstP">
+            El proceso comienza con la entrega de la materia prima en su estado
+            óptimo por parte del cultivador. Esta materia se prepara
+            cuidadosamente para ser colocada en una malla de diferentes micras,
+            dependiendo del nivel de filtración deseado, o en un papel
+            siliconado especializado. A continuación, se somete a un prensado
+            utilizando equipos hidráulicos de alta precisión.
+            <br />
+          </p>
+          <img src="../../img/Rosin-Press-Pure-Pressure-.webp" alt="" />
+        </div>
+
+        <p className="secondP">
           Durante este paso, se aplica una presión controlada en conjunto con
           una temperatura específica, cuidadosamente ajustada para maximizar el
           rendimiento y preservar la integridad de los compuestos activos. Este
@@ -23,7 +29,8 @@ const Services = () => {
           de la más alta calidad, garantizando un producto puro, limpio y sin
           contaminantes, ideal para el consumo.
           <br />
-          <p></p>
+        </p>
+        <p className="thirtyP">
           El resultado final es un extracto que conserva todo el perfil de
           terpenos y cannabinoides esenciales, logrando una experiencia superior
           tanto en sabor como en efectos. Este método asegura una extracción sin
