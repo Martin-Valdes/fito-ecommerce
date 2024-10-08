b270a0489814362a6c17797605cfee703e759074
fernando valdes <valdesmartin@gmail.com>
2024-10-08 10:47:22 -0300

    Contact Component its ok for the production

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,27 +5,25 @@ import "./Contact.scss";
 const Contact = () => {
   return (
     <div className="contactContainer">
-      <div className="back">
-      </div>
-      <div className="filter"></div>
+      <div className="back"></div>
       <div className="new">
-          <section className="blocksContainer">
-            <div className="block">
-              <img src="" alt="" />
-              <h2>Email</h2>
-              <p>fitogrowshop@gmail.com</p>
-            </div>
-            <div className="block">
-              <img src="" alt="" />
-              <h2>Direccion</h2>
-              <p>Toledo  Canelones 275 Bis.</p>
-            </div>
-            <div className="block">
-              <img src="" alt="" />
-              <h2>Telefono</h2>
-              <p>(+598) 094 588 350</p>
-            </div>
-          </section>
+        <section className="blocksContainer">
+          <div className="block">
+            <img src="../../img/emailPng.png" alt="" />
+            <h2>Email</h2>
+            <p>fitogrowshop@gmail.com</p>
+          </div>
+          <div className="block">
+            <img src="../../img/ubicPng.png" alt="" />
+            <h2>Direccion</h2>
+            <p>Toledo Canelones 275 Bis.</p>
+          </div>
+          <div className="block">
+            <img src="../../img/phonePng.png" alt="" />
+            <h2>Telefono</h2>
+            <p>(+598) 094 588 350</p>
+          </div>
+        </section>
         <section className="formContainer">
           <div className="titleContainer">
             <h1>Envianos tu consulta</h1>
